9e3b36e5d687a107b488f2fa5665c24d9c3dd027
Tasnim Lia <liatasnimlia@gmail.com>
2025-09-22 13:21:07 +0600

    Update Numerical-Assignment.py

--- Numerical-Assignment.py
+++ Numerical-Assignment.py
@@ -58,6 +58,14 @@ for i in range(N):
    rel_error_euler.append(abs(y_next_e - y_ex) / y_ex)
     rel_error_rk4.append(abs(y_next_r - y_ex) / y_ex) 
 
+# Accuracy and stability notes for this step
+    accuracy_note_list.append('Euler: less accurate; RK4: highly accurate')
+    stability_note_list.append('Euler stable if h < 1; RK4 more stable')
+
+
+
+
+
 
     
     
