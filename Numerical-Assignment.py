39e79fe54c46f5d0a5f81eff5b34f65a03251e6f
nusratjahannupur <nusratjahan231103@gmail.com>
2025-09-22 13:08:52 +0600

    Update Numerical-Assignment.py

--- Numerical-Assignment.py
+++ Numerical-Assignment.py
@@ -66,6 +66,10 @@ for i in range(N):
  # Update x
     x_val.append(x_next)
 
+# Print Table Header
+print(f"{'x':<6}{'Exact y':<12}{'Euler y':<12}{'Euler Abs Err':<15}{'Euler Rel Err':<15}"
+      f"{'RK4 y':<12}{'RK4 Abs Err':<15}{'RK4 Rel Err':<15}{'Notes':<40}")
+
 
 
 
